e2bb73a2b355ba105260a92ec274df0ee1b21026
guohw <guohw@codoon.com>
2020-06-05 10:19:31 +0800

    add start project name

---
+++ start_project.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+ProjectName=""
+ProjectPath=""
+
+while getopts ":n:p" opt
+do
+    case $opt in
+    n)
+        ProjectName=$OPTARG;;
+    p)
+        ProjectPath=$OPTARG;;
+    ?)
+        echo "-n project name"
+        echo "-p project path"
+        exit 1;;
+    esac
+done
+
+if [ -z "$ProjectName" ]; then
+    echo "must input project name"
+    exit 1;
+fi
+
+echo "project name: $ProjectName"
+
+if [ -z "$ProjectPath" ]; then
+    echo "use default project path: ./$ProjectName/"
+    ProjectPath="./$ProjectName/"
+fi
+
+echo "project path: $ProjectPath"
+
+mkdir "$ProjectPath"
+
+echo "start copy src to $ProjectPath"
+rsync -av --exclude $ProjectPath --exclude .git --exclude .gitignore --exclude .idea --exclude .DS_Store --exclude go-svr-template ./ $ProjectPath
+cd $ProjectPath
+rm -rf start_project.sh $ProjectName go.mod go.sum ./log/*
+go mod init $ProjectName
+sed -i "" "s/go-svr-template/$ProjectName/g" `grep go-svr-template -rl .`
+sed -i "" "s/TemplateServer/$ProjectName/g" `grep TemplateServer -rl .`
